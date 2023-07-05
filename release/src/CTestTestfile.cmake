c5eaafa683ef473b9fe6e403f6711b78b12a8a2c
Vigneshwar Rajesh <67299496+Vigne-hub@users.noreply.github.com>
2023-07-05 14:41:03 -0400

    delete

--- release/src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/vgnsh/OneDrive - University of Toronto/Marga_files/ML_docs/Vignesh_tries/hybridmc/src
-# Build directory: /mnt/c/Users/vgnsh/OneDrive - University of Toronto/Marga_files/ML_docs/Vignesh_tries/hybridmc/release/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(hybridmc_test "/mnt/c/Users/vgnsh/OneDrive - University of Toronto/Marga_files/ML_docs/Vignesh_tries/hybridmc/release/hybridmc_test")
-set_tests_properties(hybridmc_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/vgnsh/OneDrive - University of Toronto/Marga_files/ML_docs/Vignesh_tries/hybridmc/src/CMakeLists.txt;15;add_test;/mnt/c/Users/vgnsh/OneDrive - University of Toronto/Marga_files/ML_docs/Vignesh_tries/hybridmc/src/CMakeLists.txt;0;")
