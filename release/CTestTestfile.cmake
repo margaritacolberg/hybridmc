dfdb91250baee1706a2b34c0f6ab2305855560f9
Vigneshwar Rajesh <67299496+Vigne-hub@users.noreply.github.com>
2023-07-14 12:42:26 -0400

    Delete release directory

--- release/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cptg/u4/jmschofi/students/vignesh/hybridmc
-# Build directory: /cptg/u4/jmschofi/students/vignesh/hybridmc/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- release/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cptg/u4/jmschofi/students/vignesh/hybridmc/src
-# Build directory: /cptg/u4/jmschofi/students/vignesh/hybridmc/release/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
